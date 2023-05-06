2c0bd47612566a96b82874bccc047788a7fc9475
Fly0w <florian.budniewski@hotmail.com>
2023-05-06 18:53:18 +0900

    updated resumé

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavButton from './NavButton';
 import './NavBar.css';
-import Resume from "../img/Résumé.pdf"
+import ResumeENG from "../img/ResumeENG.pdf"
 import Language from './Language';
 
 function NavBar(props) {
@@ -11,7 +11,7 @@ function NavBar(props) {
                 <NavButton link={'AboutMe'} text={text.aboutMe} />
                 <NavButton link={'Projects'} text={text.projects}/>
                 <NavButton link={'Contact'} text={text.contact}/>
-                <a href={Resume} download target="_blank" rel="noreferrer">{text.resume}</a>
+                <a href={ResumeENG} download target="_blank" rel="noreferrer">{text.resume}</a>
                 
                 <Language
                 currentLanguage={lang}
