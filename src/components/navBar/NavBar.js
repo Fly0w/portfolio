e3d118a582296241966db7376dd57ef4796824e8
Fly0w <florian.budniewski@hotmail.com>
2023-05-16 18:10:27 +0900

    added Japanese Resumé

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavButton from './NavButton';
 import './NavBar.css';
 import ResumeENG from "../img/ResumeENG.pdf"
+import ResumeJAP from "../img/ResumeJAP.pdf"
 import Language from './Language';
 
 function NavBar(props) {
@@ -11,8 +12,10 @@ function NavBar(props) {
                 <NavButton link={'AboutMe'} text={text.aboutMe} />
                 <NavButton link={'Projects'} text={text.projects}/>
                 <NavButton link={'Contact'} text={text.contact}/>
-                <a href={ResumeENG} download target="_blank" rel="noreferrer">{text.resume}</a>
-                
+                {(lang === "en" || lang === "fr") 
+                  ? <a href={ResumeENG} download target="_blank" rel="noreferrer">{text.resume}</a> 
+                  : <a href={ResumeJAP} download target="_blank" rel="noreferrer">{text.resume}</a>
+                }
                 <Language
                 currentLanguage={lang}
                 onChangeLanguage={handleChangeLanguage}
